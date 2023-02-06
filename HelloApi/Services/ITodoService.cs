a07dc7de7c699b0feb1339c9462734e70e7caa9d
JX <>
2023-02-06 23:13:52 +0800

    add empty Todo services

---
+++ HelloApi/Services/ITodoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HelloApi.Models;
+
+namespace HelloApi.Services
+{
+	public interface ITodoService
+	{
+        IEnumerable<TodoItem> GetAll();
+
+        TodoItem GetById(long id);
+
+        TodoItem Add(TodoItem todoItem);
+        
+        void Remove(long id);
+    }
+}
+
---
+++ HelloApi/Services/TodoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HelloApi.Models;
+
+namespace HelloApi.Services
+{
+	public class TodoService : ITodoService
+	{
+		public TodoService()
+		{
+		}
+
+        public TodoItem Add(TodoItem todoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoItem GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
