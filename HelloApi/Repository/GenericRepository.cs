517d7c6bfdc4bf498d212cf9a83ec4babf258f21
JX <>
2023-02-11 16:55:36 +0800

    update generic repo

--- HelloApi/Repository/GenericRepository.cs
+++ HelloApi/Repository/GenericRepository.cs
@@ -1,30 +1,59 @@
 ﻿using System;
+using System.Linq.Expressions;
 using HelloApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelloApi.Repository
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         protected readonly TodoContext _context;
+        internal readonly DbSet<T> _table;
+
         public GenericRepository(TodoContext context)
         {
             _context = context;
+            _table = context.Set<T>();
         }
+
         public void Add(T entity)
         {
-            _context.Set<T>().Add(entity);
+            _table.Add(entity);
         }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _table.AddRange(entities);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _table.Where(expression);
+        }
+
         public IEnumerable<T> GetAll()
         {
-            return _context.Set<T>().ToList();
+            return _table.ToList();
         }
-        public T GetById(int id)
+
+        public T? GetById(int id)
         {
-            return _context.Set<T>().Find(id);
+            return _table.Find(id);
         }
+
+        public void Update(T entity)
+        {
+            _table.Update(entity);
+        }
+
         public void Remove(T entity)
         {
-            _context.Set<T>().Remove(entity);
+            _table.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _table.RemoveRange(entities);
         }
     }
 }
--- HelloApi/Repository/IGenericRepository.cs
+++ HelloApi/Repository/IGenericRepository.cs
@@ -1,11 +1,18 @@
-﻿namespace HelloApi.Repository
+﻿using System.Linq.Expressions;
+
+namespace HelloApi.Repository
 {
     public interface IGenericRepository<T> where T : class
     {
-        T GetById(int id);
+        T? GetById(int id);
         IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
         void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
         void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        
     }
 }
 
